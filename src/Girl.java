2d0bd9a47eb5ea6ef2e474347dc6aa8430e48f2f
Dmitriy Lois <epaman@rambler.ru>
2014-07-10 21:17:40 +0300

    commit unit3

---
+++ src/Girl.java
@@ -0,0 +1,17 @@
+
+public class Girl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String gfriend="Masha";
+				if (gfriend=="Masha"){System.out.println("True1");}
+				
+				String gfriend1 = new String ("Natasha");
+				String gfriend2 = new String ("Natasha");
+				
+				if (gfriend1==gfriend2){System.out.println("True2");}
+		
+	}
+
+}
---
+++ src/HelloWorld_new.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld_new {
+
+	public static void main(String[] args) {
+		String t = args[0];
+		System.out.println("Hello "+ t);
+
+	}
+
+}
